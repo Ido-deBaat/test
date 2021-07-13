593404744e9e8598979dedb2edd7709a023071cf
Ido de Baat <idodebaat@mbp-ido.local>
2021-07-13 14:25:07 +0200

    animating mobile header
    it's working.. but not neat yet

--- docs/menuScroll.js
+++ docs/menuScroll.js
@@ -1,47 +1,96 @@
+// vars
 var scrolled;
 var st_last = 0;
-var scroll_delta = 5;
+var scroll_direction;
+var scroll_delta = 24;
 var height_navbar = $('.menu-container').outerHeight();
 
-$(window).scroll(function(event) {
-    
-    scrolled = true;
-    
-});
+$(window).scroll(function(event) {scrolled = true;});
 
+// funs
 setInterval(function() {
-    
+
     if (scrolled) {
-        scroll_action();
+        set_scroll_dir();
         scrolled = false;
     }
-    
-}, 50);
 
-function scroll_action() {
+}, 250);
+
+
+function heads_up() {    
     
+    console.log(scroll_direction);
+    $('header').removeClass('nav-down').addClass('nav-up');
+    
+};
+
+function heads_down() {    
+    
+    console.log(scroll_direction);
+    $('header').removeClass('nav-up').addClass('nav-down');
+    
+};
+
+function set_scroll_dir() {
+
     var st_current = $(this).scrollTop();
-    
-    if (Math.abs(st_last - st_current) <= scroll_delta) {
-        
-        if (st_current > st_last && 
-            st_current > height_navbar) {
-            
-            $('header').removeClass('nav-up').addClass('nav-down');
-//            alert('down');
-            
-        } else {
-            
-            if (st_current + $(window).height() <  $(document).height()) {
-//                $('header').removeClass('header-up').addClass('header-down');
-                $('header').removeClass('nav-down').addClass('nav-up');
-//                alert('up');
-            }
-            
+
+    if (Math.abs(st_current - st_last) >= scroll_delta) {
+
+        if (st_current > st_last && scroll_direction != 'down' &&
+            st_current > 0) {
+
+            scroll_direction = 'down';
+            heads_down();
+
+        } else if (st_current < st_last && scroll_direction != 'up') {
+
+            scroll_direction  = 'up';
+            heads_up();
+
         }
-        
-    }
-    
+
+    };
+
     st_last = st_current;
-    
-}
+
+};
+
+//function scroll_action() {
+//    
+//    var st_current = $(this).scrollTop();
+//    
+//    if (Math.abs(st_last - st_current) <= scroll_delta) {
+//        
+//        if (st_current > st_last && 
+//            st_current > height_navbar) {
+//            
+//            $('header').removeClass('nav-up').addClass('nav-down');
+////            alert('down');
+//            
+//        } else {
+//            
+//            if (st_current + $(window).height() <  $(document).height()) {
+////                $('header').removeClass('header-up').addClass('header-down');
+//                $('header').removeClass('nav-down').addClass('nav-up');
+////                alert('up');
+//            }
+//            
+//        }
+//        
+//    }
+//    
+//    st_last = st_current;
+//    
+//}
+
+
+
+
+
+
+
+
+
+
