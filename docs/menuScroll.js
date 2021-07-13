368641458c978b14bf63fba5c5013de70e834cc7
Ido de Baat <idodebaat@mbp-ido.local>
2021-07-13 15:03:48 +0200

    better

--- docs/menuScroll.js
+++ docs/menuScroll.js
@@ -1,4 +1,3 @@
-// vars
 var scrolled;
 var st_last = 0;
 var scroll_direction;
@@ -7,7 +6,6 @@ var height_navbar = $('.menu-container').outerHeight();
 
 $(window).scroll(function(event) {scrolled = true;});
 
-// funs
 setInterval(function() {
 
     if (scrolled) {
@@ -17,20 +15,9 @@ setInterval(function() {
 
 }, 250);
 
+function heads_up() {$('header').removeClass('nav-down').addClass('nav-up');};
 
-function heads_up() {    
-    
-    console.log(scroll_direction);
-    $('header').removeClass('nav-down').addClass('nav-up');
-    
-};
-
-function heads_down() {    
-    
-    console.log(scroll_direction);
-    $('header').removeClass('nav-up').addClass('nav-down');
-    
-};
+function heads_down() {$('header').removeClass('nav-up').addClass('nav-down');};
 
 function set_scroll_dir() {
 
@@ -41,12 +28,10 @@ function set_scroll_dir() {
         if (st_current > st_last && scroll_direction != 'down' &&
             st_current > 0) {
 
-            scroll_direction = 'down';
             heads_down();
 
         } else if (st_current < st_last && scroll_direction != 'up') {
 
-            scroll_direction  = 'up';
             heads_up();
 
         }
@@ -56,41 +41,3 @@ function set_scroll_dir() {
     st_last = st_current;
 
 };
-
-//function scroll_action() {
-//    
-//    var st_current = $(this).scrollTop();
-//    
-//    if (Math.abs(st_last - st_current) <= scroll_delta) {
-//        
-//        if (st_current > st_last && 
-//            st_current > height_navbar) {
-//            
-//            $('header').removeClass('nav-up').addClass('nav-down');
-////            alert('down');
-//            
-//        } else {
-//            
-//            if (st_current + $(window).height() <  $(document).height()) {
-////                $('header').removeClass('header-up').addClass('header-down');
-//                $('header').removeClass('nav-down').addClass('nav-up');
-////                alert('up');
-//            }
-//            
-//        }
-//        
-//    }
-//    
-//    st_last = st_current;
-//    
-//}
-
-
-
-
-
-
-
-
-
-
